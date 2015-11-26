b580d3d948b205b3eec72d7078e8fd7a16d7a6db
Zee Nastalski <zbigniew.nastalski@jeppesen.com>
2015-11-26 16:12:02 +1100

    refactored to use custom directives

--- tuts/angular/GemStore/app.js
+++ tuts/angular/GemStore/app.js
@@ -1,108 +1,147 @@
-(function(){
-    var app = angular.module('store', [ ]);
+(function () {
+    var app = angular.module('gemStore', []);
 
-    app.controller('StoreController', function() {
+
+
+    app.controller('StoreController', function () {
         this.products = gems;
     });
-	
-	
-    app.controller('PanelController', function() {
-        this.tab = 1;
-		
-        this.selectTab = function(setTab) {
-            this.tab = setTab;
-        };
-		
-        this.isSelected = function(checkTab) {
-            return checkTab === this.tab;
+
+
+    app.directive('productGallery', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-gallery.html',
+            controller: function () {
+                this.current = 0;
+                this.setCurrent = function (imageNumber) {
+                    this.current = imageNumber || 0;
+                };
+            },
+            controllerAs: 'gallery'
         };
     });
-	
-	
-	
-    app.controller('TabController', function(){
-        this.tab = 1;
 
-        this.setTab = function(newValue){
-            this.tab = newValue;
-        };
 
-        this.isSet = function(tabName){
-            return this.tab === tabName;
+    app.directive('productTabs', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-tabs.html',
+            controller: function () {
+                this.tab = 1;
+
+                this.isSet = function (checkTab) {
+                    return this.tab === checkTab;
+                };
+
+                this.setTab = function (setTab) {
+                    this.tab = setTab;
+                };
+            },
+            controllerAs: 'tab'
         };
     });
-	
 
-	
-    app.controller('ReviewController', function() {
+
+    app.controller("ReviewController", function () {
+
         this.review = {};
-		
-        this.addReview = function(product) {
-            this.review.createdOn = Date.now();
+
+        this.addReview = function (product) {
             product.reviews.push(this.review);
             this.review = {};
         };
+
     });
-	
-	
-    
-    app.directive('productTitle', function () {
+
+    app.directive("productDescription", function () {
         return {
             restrict: 'E',
-            templateUrl: 'product-title.html'
+            templateUrl: "product-description.html"
         };
     });
-    
-    
 
-    /*
-    app.directive('productTitle', function () {
+    app.directive("productReviews", function () {
         return {
-            restrict: 'A',
-            templateUrl: 'product-title.html'
+            restrict: 'E',
+            templateUrl: "product-reviews.html"
         };
     });
-	*/
 
-	var gems = [
-		{
-			name: "Craponite",
-			price: 2.95,
-			description: "24 ounces of pure crap",
-			canPurchase: true,
-			image: 'gem.png',
-			reviews: [
-				{
-					stars: 5,
-					body: "love it",
-					author: "aaa@@gmail.com"					
-				},
-				{
-					stars: 1,
-					body: "hate it",
-					author: "aaa@@gmail.com"					
-				},
-			],
-		},
-		{
-			name: "Loochohedron",
-			price: 3,
-			description: "Tough like a diamond",
-			canPurchase: true,
-			image: 'gem.png',
-			reviews: [
-				{
-					stars: 5,
-					body: "pretty..",
-					author: "aaa@@gmail.com"					
-				},
-				{
-					stars: 1,
-					body: "not so good",
-					author: "aaa@@gmail.com"					
-				},
-			],
-		}
-	];
-	
+    app.directive("productSpecs", function () {
+        return {
+            restrict: "A",
+            templateUrl: "product-specs.html"
+        };
+    });
+
+
+
+    var gems = [
+      {
+          name: 'Azurite',
+          description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+          shine: 8,
+          price: 110.50,
+          rarity: 7,
+          color: '#CCC',
+          faces: 14,
+          images: [
+            "images/gem.png",
+          ],
+          reviews: [{
+              stars: 5,
+              body: "I love this gem!",
+              author: "joe@example.org"
+          }, {
+              stars: 1,
+              body: "This gem sucks.",
+              author: "tim@example.org"
+          }]
+      }, {
+          name: 'Bloodstone',
+          description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+          shine: 9,
+          price: 22.90,
+          rarity: 6,
+          color: '#EEE',
+          faces: 12,
+          images: [
+            "images/gem.png",
+          ],
+          reviews: [{
+              stars: 3,
+              body: "I think this gem was just OK, could honestly use more shine, IMO.",
+              author: "JimmyDean@example.org"
+          }, {
+              stars: 4,
+              body: "Any gem with 12 faces is for me!",
+              author: "gemsRock@example.org"
+          }]
+      }, {
+          name: 'Zircon',
+          description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+          shine: 70,
+          price: 1100,
+          rarity: 2,
+          color: '#000',
+          faces: 6,
+          images: [
+            "images/gem.png",
+          ],
+          reviews: [{
+              stars: 1,
+              body: "This gem is WAY too expensive for its rarity value.",
+              author: "turtleguyy@example.org"
+          }, {
+              stars: 1,
+              body: "BBW: High Shine != High Quality.",
+              author: "LouisW407@example.org"
+          }, {
+              stars: 1,
+              body: "Don't waste your rubles!",
+              author: "nat@example.org"
+          }]
+      }
+    ];
 })();
