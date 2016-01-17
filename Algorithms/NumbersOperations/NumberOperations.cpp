9e79caae199736b68c0a3883a403fa50210cda04
nastalski@gmail.com <nastalski@gmail.com>
2016-01-17 16:20:53 +1100

    NumberOperations

--- Algorithms/NumbersOperations/NumberOperations.cpp
+++ Algorithms/NumbersOperations/NumberOperations.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
 #define N 5
 
 /*
@@ -9,17 +11,75 @@
 using namespace std;
 
 
-bool YES = false;
-int cnt = 0;
-int perms = 0;
-
-void swap(int arr[], int i, int j)
+template<class T> class VectorCombinatorics
 {
-	int tmp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = tmp;
-}
+	typedef bool(*CallbackType)(vector<T>&);
 
+	public:
+		VectorCombinatorics() {};
+		virtual ~VectorCombinatorics() {};
+		
+		// makes a callback for every permutation. 
+		// Permutating stops when callback returns true.
+		void permutations(vector<T> vec, CallbackType callback)
+		{
+			// vector passed by value here. Don't want to modify original vector.
+			permutateRecursively(vec, 0, callback);
+		}
+
+		void combinations(vector<T> values, int length, CallbackType callback)
+		{
+			vector<T> combination(length);
+			combinationsRecursively(values, combination, 0, callback);
+						
+		}
+
+	protected:
+		
+		void combinationsRecursively(const vector<T> &values, vector<T> &combination, int from, CallbackType callback)
+		{
+			if (from == combination.size())
+			{
+				callback(combination);
+				return;
+			}
+
+			for (int v : values)
+			{
+				combination[from] = v;
+				combinationsRecursively(values, combination, from + 1, callback);
+			}
+
+		}
+
+		void permutateRecursively(vector<T> &vec, int from, CallbackType callback)
+		{
+			if (from == vec.size())
+			{
+				callback(vec);
+				return;
+			}
+
+			for (int i = from; i < vec.size(); i++)
+			{
+				swap(vec, i, from);
+				permutateRecursively(vec, from + 1, callback);
+				swap(vec, i, from);
+			}
+		}
+
+		void swap(vector<T> &vec, int i, int j)
+		{
+			int tmp = vec[i];
+			vec[i] = vec[j];
+			vec[j] = tmp;
+		}
+};
+
+
+
+bool YES = false;
+vector<int> currentPermutation;
 
 int apply_op(int a, int b, int op)
 {
@@ -28,7 +88,8 @@ int apply_op(int a, int b, int op)
 	if (op == 2) return a * b;
 }
 
-string print_op(int op)
+
+string opCodeToString(int op)
 {
 	if (op == 0) return "+";
 	if (op == 1) return "-";
@@ -36,78 +97,85 @@ string print_op(int op)
 }
 
 
-
-void test_result(int arr[], int ops[])
+void printOperation(const vector<int> nums, const vector<int> ops, int result)
 {
-	
-	cout << arr[0] << print_op(ops[0]) << arr[1] << print_op(ops[1]) << arr[2] << print_op(ops[2]) << arr[3] << print_op(ops[3]) << arr[4];
-
-	int value = arr[0];
-	for (int i = 1; i < N; i++)
+	for (int i = 0; i < ops.size(); i++)
 	{
-		value = apply_op(value, arr[i], ops[i - 1]);
+		cout << nums[i] << opCodeToString(ops[i]);
 	}
 
-	cout << "=" << value << endl;
+	cout << nums[nums.size() - 1] << "=" << result << endl;
+}
+
+void test_result(const vector<int> nums, const vector<int> ops)
+{	
+
+	int value = nums[0];
+	for (int i = 1; i < N; i++)
+	{
+		value = apply_op(value, nums[i], ops[i - 1]);
+	}
+
+	//printOperation(nums, ops, value);
 	
 	if (value == 42) { 
 		YES = true;
 	}
-	
-	cnt++;
-	
 }
 
 
-void test_combinations_of_operations(int arr[])
+bool nextCombination(vector<int> &comb)
 {
-	perms++;
-	/* having a permutation of number we need to test all possible combinations of operations
-	convention
-		0 plus
-		1 minus
-		2 times
-	*/
-	
+	test_result(currentPermutation, comb);
 
-	int ops[4];
-
-	for (ops[0] = 0; ops[0] < 2; ops[0]++)
-		for (ops[1] = 0; ops[1] < 2; ops[1]++)
-			for (ops[2] = 0; ops[2] < 2; ops[2]++)
-				for (ops[3] = 0; ops[3] < 2; ops[3]++)
-				{
-					test_result(arr, ops);
-				}
-		
+	return false;
 }
 
-void permutate(int arr[], int from)
+bool nextPermutation(vector<int> &perm)
 {
+	currentPermutation = perm;
 	
-	if (from == N - 1)
-	{
-		test_combinations_of_operations(arr);
-	}
+	VectorCombinatorics<int> combinatorics;
 
-	for (int i = from; i < N; i++)
-	{
-		swap(arr, i, from);
-		permutate(arr, from + 1);
-		swap(arr, i, from);
-	}
+	combinatorics.combinations(vector<int> {0, 1, 2}, N-1, nextCombination);
+
+	return false;
 }
 
+/*
 int main() {
 
-	int arr[N];
-	for (int i = 0; i < N; i++) { cin >> arr[i]; };
+	vector<int> arr(N);
+	for (int i = 0; i < N; i++) { 
+		cin >> arr[i]; 
+	}
 
-	permutate(arr, 0);
+	VectorCombinatorics<int> combinatorics;
+	
+	combinatorics.permutations(arr, nextPermutation);
+
+	if (YES) cout << "YES" << endl;
+	else cout << "NO" << endl;
+	return 0;
+}
+*/
+
+int main(int argc, char *argv[]) {
+	
+	ifstream stream(argv[1]);
+	string line;
+	vector<int> arr(N);
+
+	while (getline(stream, line)) {		
+		sscanf(line.c_str(), "%d %d %d %d %d", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4]);
+	}
+
+	VectorCombinatorics<int> combinatorics;
+    combinatorics.permutations(arr, nextPermutation);
 
 	if (YES) cout << "YES";
-	else cout << "NO";
+	else cout << "NO" ;
+	
 	return 0;
-
 
 }
