1370ae7f7fc092f720dfd587221955e219edb7c2
nastalski@gmail.com <nastalski@gmail.com>
2016-01-18 21:43:48 +1100

    adding c# excercise solution

---
+++ CSharpExam/Threads/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+
+        public static void ThreadMethod()
+        {
+            for(int i = 0; i<10; i++)
+            {
+                System.Console.WriteLine("Thread " + i);
+                Thread.Sleep(0);
+            }
+                
+        }
+
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for(int i=0; i<10; i++)
+            {
+                System.Console.WriteLine("Main thread " + i);
+                Thread.Sleep(0);
+            }
+        }
+
+    }
+}
