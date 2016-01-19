5711e1c019ede111fca20fe2bde27e2023c95c74
nastalski@gmail.com <nastalski@gmail.com>
2016-01-20 08:20:55 +1100

    c#

---
+++ CSharpExam/Threads/StartingThread.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+
+namespace Threads
+{
+    class StartingThread
+    {
+        static bool stop = false;
+
+        public static void ThreadMethod(object o)
+        {
+            int number = (int)o;
+
+            while(!stop)
+            {
+                System.Console.WriteLine("Thread printing " + number);
+                Thread.Sleep(0);
+            }
+                
+        }
+
+
+        public static void Run(string[] args)
+        {
+            // ThreadStart is a wrapper on the thread method
+            // hread t = new Thread(new ThreadStart(ThreadMethod));
+
+            // ParameterizedThreadStart allows for a start method to have one arg of type object
+
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            
+            //t.IsBackground = true;
+            t.Start(10);
+
+            System.Console.ReadKey();
+            stop = true;
+            t.Join();
+
+        }
+
+    }
+}
---
+++ CSharpExam/Threads/ThreadStatic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    class ThreadStatic
+    {
+        [ThreadStatic]
+        private static int countner;
+
+        public static void Run(string[] args)
+        {
+            new System.Threading.Thread(() =>
+               {
+                   for(int i=0; i<10; i++)
+                   {
+                       countner++;
+                       Console.WriteLine("Thread A couter=" + countner);                  
+
+                   }
+               }).Start();
+
+
+            new System.Threading.Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    countner++;
+                    Console.WriteLine("Thread B couter=" + countner);
+
+                }
+            }).Start();
+
+
+            Console.ReadKey();
+        }
+    }
+}
