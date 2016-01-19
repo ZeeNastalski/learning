3528b1320d4193cfab399a4832ded8449677068c
nastalski@gmail.com <nastalski@gmail.com>
2016-01-20 08:18:35 +1100

    c#

--- CSharpExam/Threads/Program.cs
+++ CSharpExam/Threads/Program.cs
@@ -1,38 +1,20 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Threads
 {
     class Program
     {
-        static bool stop = false;
-
-        public static void ThreadMethod(object o)
-        {
-            int number = (int)o;
-
-            while(!stop)
-            {
-                System.Console.WriteLine("Thread printing" + number);
-                Thread.Sleep(0);
-            }
-                
-        }
-
-
         static void Main(string[] args)
         {
-            // ThreadStart is a wrapper on the thread method
-            // ParameterizedThreadStart allows for a start method to have one arg of type object
-            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            //StartingThread.Run(args);
+            ThreadStatic.Run(args);
+
+
             
-            //t.IsBackground = true;
-            t.Start(10);
-
-            System.Console.ReadKey();
-            stop = true;
-            t.Join();
-
         }
-
     }
 }
