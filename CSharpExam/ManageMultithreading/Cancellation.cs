a4a43b4e7e2f8774aef33870c6d15006dd15d817
nastalski@gmail.com <nastalski@gmail.com>
2016-02-08 21:33:26 +1100

    multithreading

---
+++ CSharpExam/ManageMultithreading/Cancellation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace ManageMultithreading
+{
+    [TestClass]
+    public class Cancellation
+    {
+
+        [TestMethod]
+        public void CancelingTask()
+        {
+            CancellationTokenSource cancelationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancelationTokenSource.Token;
+
+            Task task = Task.Run(() => {
+
+                while (!token.IsCancellationRequested)
+                {
+                    Console.Write("*");
+                    Thread.Sleep(500);
+                }
+
+                //token.ThrowIfCancellationRequested();
+                throw new OperationCanceledException();
+
+            }, token).ContinueWith( (t) =>
+            {
+
+                //t.Exception.Handle((e) => { Console.WriteLine(e.Message); return true; });
+                Console.WriteLine("Continuation");
+                 
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+
+
+            try
+            {
+                Thread.Sleep(3000);
+                cancelationTokenSource.Cancel();
+
+                task.Wait();
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("Handling aggr exception " + e.InnerExceptions[0].Message);
+            }
+        }
+
+    }
+}
---
+++ CSharpExam/ManageMultithreading/LockObject.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ManageMultithreading
+{
+    [TestClass]
+    public class LockObject
+    {
+
+
+
+        [TestMethod]
+        public void NoLocking()
+        {
+            int n = 0;
+
+            Task up = Task.Run(() => {
+                for (int i = 0; i < 1000000; i++) n++;
+            });
+
+            for (int i = 0; i < 1000000; i++) n--;
+
+            up.Wait();
+
+            Console.WriteLine(n);
+            Assert.AreNotEqual(0, n);
+        }
+
+
+
+
+        [TestMethod]
+        public void LocObject()
+        {
+            int n = 0;
+            object _lock = new object();
+
+            Task up = Task.Run(() => {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    lock (_lock)
+                    {
+                        n++;
+                    }
+                }
+            });
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (_lock)
+                {
+                    n--;
+                }
+            }
+
+            up.Wait();
+
+            Console.WriteLine(n);
+            Assert.AreEqual(0, n);
+        }
+
+
+
+  
+
+    }
+}
