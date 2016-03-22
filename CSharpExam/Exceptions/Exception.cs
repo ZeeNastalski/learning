26773a85bf028ec23c34a4d5719d4300a2a66200
Zee Nastalski <nastalski@gmail.com>
2016-03-22 15:35:59 +1100

    Exceptions

---
+++ CSharpExam/Exceptions/Exception.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exceptions
+{
+    [TestClass]
+    public class Exception
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void ExceptionDispatchInfoExample()
+        {
+            ExceptionDispatchInfo possibleException = null;
+
+            try
+            {
+                string s = "aaa";
+                int.Parse(s);
+            }
+            catch (FormatException ex)
+            {
+                possibleException = ExceptionDispatchInfo.Capture(ex);
+            }
+
+            if (possibleException != null)
+            {
+                possibleException.Throw();
+            }
+            
+        }
+    }
+}
