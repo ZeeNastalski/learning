cb912ed11bd9a2de746705186b3feecdef3355f4
Zee Nastalski <nastalski@gmail.com>
2018-11-25 16:22:48 +1100

    wpf demo'

---
+++ Wpf/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfDemo.Models;
+
+namespace WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Zee's window";
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;           
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var personsList = (ObservableCollection<Person>)lvNames.ItemsSource;
+            personsList.Add(new Person() { Name = "Steve", Age = 13 });
+        }
+
+        private void BtnChange_OnClick(object sender, RoutedEventArgs e)
+        {
+            var personsList = (ObservableCollection<Person>)lvNames.ItemsSource;
+            personsList[0].Name = "Changed";
+        }
+    }
+}
---
+++ Wpf/Models/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WpfDemo.Annotations;
+
+namespace WpfDemo.Models
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string name;
+        private int age;
+
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                this.name = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
