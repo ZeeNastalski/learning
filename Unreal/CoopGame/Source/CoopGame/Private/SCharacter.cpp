a0dd021283555a0969f5bbd088bfc7907f4c6d6d
Zee Nastalski <nastalski@gmail.com>
2019-07-27 17:36:28 +1000

    jumping

--- Unreal/CoopGame/Source/CoopGame/Private/SCharacter.cpp
+++ Unreal/CoopGame/Source/CoopGame/Private/SCharacter.cpp
@@ -64,6 +64,8 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAxis("Turn", this, &ASCharacter::AddControllerYawInput);
 	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ASCharacter::BeginCrouch);
 	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ASCharacter::EndCrouch);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	
 
 }
 
