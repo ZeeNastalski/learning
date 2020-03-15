576fb5bcbd1401f7282dfc4f59acd7b26ec8082a
Zee Nastalski <nastalski@gmail.com>
2020-03-15 15:05:49 +1100

    nancy test

--- HttpServers/NancySelfHosted/NancySelfHosted/Program.cs
+++ HttpServers/NancySelfHosted/NancySelfHosted/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,32 @@ using Nancy.Hosting.Self;
 namespace NancySelfHosted
 {
 
+
+
     public class ApplicationBootstrapper : DefaultNancyBootstrapper
     {
+        public class ZLPathProvider : IRootPathProvider
+        {
+            public string GetRootPath()
+            {
+                return Path.GetFullPath(@"c:\Games");
+            }
+        }
+
+        ZLPathProvider pathProvider = new ZLPathProvider();
+        protected override IRootPathProvider RootPathProvider
+        {
+            get
+            {
+                return pathProvider;
+            }
+        }
+
         protected override void ConfigureConventions(NancyConventions nancyConventions)
         {
-            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("static", @"d:/tmp/html"));
+            
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("download", @"/../games"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("games", @"/../zerolatency/launcher/games"));
             base.ConfigureConventions(nancyConventions);
         }
     }
@@ -23,18 +45,21 @@ namespace NancySelfHosted
     {
         public MainModule()
         {
-            Get("/",  args => { return "Hello World"; });
+            Get("/hello", args =>
+            {
+                return "Hello World";
+            });
         }
     }
 
 
     class Program
     {
-
         static void Main(string[] args)
         {
             HostConfiguration config = new HostConfiguration();
             config.UrlReservations.CreateAutomatically = true;
+            config.AllowChunkedEncoding = false;
             string uri = "http://localhost:8666";
 
             var host = new NancyHost(config, new Uri(uri));
